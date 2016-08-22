5f249589ca5a168554ca33cc7ba68277c3c249cc
lxykad <503215875@qq.com>
2016-08-22 13:55:59 +0800

    add activity

--- app/src/main/java/appbar/com/SecondActivity.java
+++ app/src/main/java/appbar/com/SecondActivity.java
@@ -1,13 +1,14 @@
 package appbar.com;
 
 import android.annotation.TargetApi;
+import android.content.Intent;
 import android.os.Build;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.transition.Explode;
-import android.transition.Fade;
-import android.transition.Slide;
+import android.view.View;
 import android.view.Window;
+import android.widget.TextView;
 
 public class SecondActivity extends AppCompatActivity {
 
@@ -18,5 +19,13 @@ public class SecondActivity extends AppCompatActivity {
         requestWindowFeature(Window.FEATURE_CONTENT_TRANSITIONS);
         getWindow().setEnterTransition(new Explode());//slide fade Explode
         setContentView(R.layout.activity_second);
+
+        TextView title = (TextView) findViewById(R.id.tv_title);
+        title.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(SecondActivity.this,ThirdActivity.class));
+            }
+        });
     }
 }
